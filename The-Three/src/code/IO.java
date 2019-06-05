763ad0c904b94926d03719116d904cc218ecdccd
shao0099876 <shao0099876@outlook.com>
2019-06-05 11:27:22 +0800

    修改编码问题

--- The-Three/src/code/IO.java
+++ The-Three/src/code/IO.java
@@ -1,39 +1,38 @@
 package code;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class IO {
 	public static String read(DataInputStream input) {
-		ArrayList<Byte> array=new ArrayList<Byte>();
-		while(true) {
-			try {
-				array.add(input.readByte());
-			} catch (EOFException e) {
-				// TODO Auto-generated catch block
-				break;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		BufferedReader reader=new BufferedReader(new InputStreamReader(input));
+		try {
+			return reader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		byte[] byte_tmp=new byte[array.size()];
-		for(int i=0;i<array.size();i++) {
-			byte_tmp[i]=array.get(i);
-		}
-		return Code.decode(byte_tmp);
+		return null;
 	}
 	public static void write(DataOutputStream output,String s) {
+		BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(output));
 		try {
-			output.write(Code.encode(s));
-			output.flush();
+			writer.write(s+"\n");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return;
+	}
+	public static int readInt(DataInputStream input) {
+		String tmp=read(input);
+		return Integer.valueOf(tmp);
 	}
 }
